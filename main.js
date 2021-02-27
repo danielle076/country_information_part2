9bd6e6451d5f9de4a69084f835a10aa91971138b
danielle076 <d.vandenakker@novi-education.nl>
2021-02-27 12:31:54 +0100

    installed files and checked files

---
+++ main.js
@@ -0,0 +1,4 @@
+// 1. Maak een nieuwe HTML-pagina aan en link daar naartoe vanaf de home-pagina. Maak ook een nieuw JavaScript bestand
+// aan en link die aan de nieuwe pagina. Koppel ook het bestaande CSS bestand hieraan.
+
+console.log("Hello")
