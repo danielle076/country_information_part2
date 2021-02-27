eff3dd346cac229e8717c987f980f53e63599293
danielle076 <d.vandenakker@novi-education.nl>
2021-02-27 13:19:19 +0100

    added landnaam en vlag aan dom

--- main.js
+++ main.js
@@ -3,18 +3,61 @@
 // console.log("Hello")
 
 // 2. Maak een functie die alle landen ophaalt en sorteert op grootte van de populatie, van laag naar hoog.
-async function getCountries(){
+// async function getCountries(){
+//     const result = await axios.get("https://restcountries.eu/rest/v2/all");
+//     console.log("RESULT:", result) // RESULT: {data: Array(250), status: 200, statusText: "", headers: {…}, config: {…},…}
+//     console.log("AXIOS:", axios) // AXIOS: ƒ (){for(var n=new Array(arguments.length),r=0;r<n.length;r++)n[r]=arguments[r];return e.apply(t,n)}
+//     // Data uit het result object halen
+//     const {data} = result;
+//     console.log("DATA:", data); // DATA: (250)[{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, etc.
+//     // Sorteer de landen in het object op population
+//     data.sort(function(a, b) {
+//         return a.population - b.population;
+//     });
+// }
+//
+// // Aanroepen functie getCountries
+// getCountries();
+
+// 3. Zorg ervoor dat er een lijst met land-namen en de bijbehorende vlag in de DOM wordt geplaatst. Gebruik hierbij
+// destructuring om de properties aan te spreken.
+// Aanroepen country-container in html
+countryListContainer = document.getElementById("country-container");
+
+async function getCountries() {
     const result = await axios.get("https://restcountries.eu/rest/v2/all");
-    console.log("RESULT:", result) // RESULT: {data: Array(250), status: 200, statusText: "", headers: {…}, config: {…},…}
-    console.log("AXIOS:", axios) // AXIOS: ƒ (){for(var n=new Array(arguments.length),r=0;r<n.length;r++)n[r]=arguments[r];return e.apply(t,n)}
     // Data uit het result object halen
     const {data} = result;
-    console.log("DATA:", data); // DATA: (250)[{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, etc.
     // Sorteer de landen in het object op population
-    data.sort(function(a, b) {
+    data.sort(function (a, b) {
         return a.population - b.population;
     });
+
+// Creëer html structuur en inhoud API array
+    data.map(function({ flag, name }) {
+        // Listitem met uniek id
+        const li = document.createElement("li");
+
+        // Vlag toevoegen
+        const flagImg = document.createElement("img");
+        console.log("FLAGIMG:", flagImg); // urls met vlag
+        flagImg.setAttribute("src", flag);
+        flagImg.setAttribute("class", "flag");
+
+        // Naam land toevoegen
+        const nameCountry = document.createElement("name-country");
+        console.log("NAME COUNTRY:", nameCountry); // lijst met naam countries, NAME COUNTRY: <name-country class="country">India</name-country>
+        nameCountry.setAttribute("class", "country");
+        nameCountry.textContent = name;
+
+        // Vlag en land toevoegen aan listitem
+        li.appendChild(flagImg);
+        li.appendChild(nameCountry);
+
+        // Listitem toevoegen aan html
+        countryListContainer.appendChild(li);
+    })
 }
 
-// Aanroepen functie countries
+// Aanroepen functie getCountries
 getCountries();
